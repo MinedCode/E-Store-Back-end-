30f054b55820ad00847347fcea05cf92e94d7a94
Gyl Monteiro <gyldeveloper@gmail.com>
2025-09-04 12:01:21 -0300

    feat: add product CRUD routes

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,11 +1,21 @@
 import { Router } from "express";
-import { getProducts } from "../controllers/productsController.js";
+import {
+  getProducts,
+  createProduct,
+  updateProduct,
+  partialUpdateProduct,
+  deleteProduct,
+} from "../controllers/productsController.js";
 
 const productsRouter = Router();
 
 //Definição das rotas aqui
 
 productsRouter.get("/", getProducts);
+productsRouter.post("/", createProduct);
+productsRouter.put("/:id", updateProduct);
+productsRouter.patch("/:id", partialUpdateProduct);
+productsRouter.delete("/:id", deleteProduct);
 
 /* Demais rotas a serem criadas 
 Exemplo: 
