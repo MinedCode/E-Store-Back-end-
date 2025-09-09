fa090d69a95f638a4d5e0007d5a98ff9272980c8
Gyl Monteiro <gyldeveloper@gmail.com>
2025-09-09 08:53:03 -0300

    feat: comment out incomplete code snippets

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,21 +1,21 @@
 import { Router } from "express";
 import {
   getProducts,
-  createProduct,
-  updateProduct,
-  partialUpdateProduct,
-  deleteProduct,
+  //createProduct,
+  //updateProduct,
+  //partialUpdateProduct,
+  //deleteProduct,
 } from "../controllers/productsController.js";
 
 const productsRouter = Router();
 
 //Definição das rotas aqui
 
+/*
 productsRouter.get("/", getProducts);
 productsRouter.post("/", createProduct);
 productsRouter.put("/:id", updateProduct);
 productsRouter.patch("/:id", partialUpdateProduct);
 productsRouter.delete("/:id", deleteProduct);
-
-
+*/
 export default productsRouter;
