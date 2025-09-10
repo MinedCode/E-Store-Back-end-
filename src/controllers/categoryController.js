6a96ab8affb95c8f27486afbf2c5d5a4b2fe84f1
Diogoalves-dev <diogoribeirodev1@gmail>
2025-09-10 02:28:26 -0300

    feat: add remaining routes

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,6 +1,6 @@
 import prisma from "../utils/prismaInstance.js";
 
-//buscar todas as categorias
+//function para buscar todas as categorias no banco de dados
 export const getCategorys = async (req, res) => {
     try{
         const categorys = await prisma.category.findMany();
@@ -10,7 +10,7 @@ export const getCategorys = async (req, res) => {
     }
 };
 
-//buscar categoria especifica por id
+//function para buscar categoria especifica por id no banco de dados
 export const getSpecificCategory = async (req, res) => {
     try {
         const{id} = req.params;
@@ -24,4 +24,50 @@ export const getSpecificCategory = async (req, res) => {
         res.status(500).json({erro: "Erro interno no servidor"});
         console.log(`erro: ${error.message}`);
     }
+};
+
+//function para inserir uma categoria no banco de dados
+export const createCategory = async (req, res) =>{
+    try{
+        const{name} = req.body;
+        const newCategory = await prisma.category.create({
+            data:{
+                name: name
+            }
+        });
+        res.status(201).send("Categoria criada com exito!");
+    }catch(error){
+        res.status(500).send("erro ao inserir no banco de dados");
+        console.log(`erro: ${error.message}`);
+    }
+};
+
+//function para atualizar uma categoria
+export const updateCategory = async (req, res) =>{
+    try {
+        const{id} = req.params;
+        const{name} = req.body;
+        const categoryUpdated = await prisma.category.update({
+            where: { id: parseInt(id) },
+            data: { name: name}
+        });
+        res.status(200).send("Categoria atualizada!");
+    } catch (error) {
+        res.status(500).send("Erro ao atualizar a categoria");
+        console.log(`erro: ${error.message}`);
+    }
+};
+
+//function para deletar uma categoria do banco de dados
+export const deleteCategory = async (req, res) => {
+  try{
+    const{id} = req.params;
+    const categoryDeleted = await prisma.category.delete({
+      where: { id: parseInt(id) }
+    });
+    res.status(204).send("categoria deletada com sucesso");
+  }catch(error){
+    res.status(500).send("erro ao deletar a categoria");
+    console.log(`erro: ${error.message}`);
+  }
 };
--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { getCategorys, getSpecificCategory } from "../controllers/categoryController.js";
+import { 
+    getCategorys,
+    getSpecificCategory,
+    createCategory,
+    updateCategory,
+    deleteCategory
+ } from "../controllers/categoryController.js";
 
 const categoryRouter = Router();
 
 categoryRouter.get("/", getCategorys);
 categoryRouter.get("/:id", getSpecificCategory);
+categoryRouter.post("/", createCategory);
+categoryRouter.put("/:id", updateCategory);
+categoryRouter.delete("/:id", deleteCategory);
 
 export default categoryRouter;
