1c6b37bb893bc9757fcdbd3049cbdc1273ade02f
Diogoalves-dev <diogoribeirodev1@gmail>
2025-09-10 08:14:52 -0300

    feat: code refactoring

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -12,11 +12,10 @@ const productsRouter = Router();
 //Definição das rotas aqui
 
 productsRouter.get("/", getProducts);
-/*
 productsRouter.get("/:id", getSpecificProduct);
 productsRouter.post("/", createProduct);
 productsRouter.put("/:id", updateProduct);
 productsRouter.delete("/:id", deleteProduct);
-*/
+
 
 export default productsRouter;
