bd774c4372ca93df765c0fe24a72429004700177
Gyl Monteiro <gyldeveloper@gmail.com>
2025-09-16 11:41:32 -0300

    feat: use bcrypt for user password hashing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,33 +1,35 @@
 import prisma from "../utils/prismaInstance.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 //function para buscar todos os usuarios do banco de dados
 export const getUsers = async (req, res) => {
-    try{
-        const users = await prisma.users.findMany();
-        res.status(200).json(users);
-    }catch(error){
-        res.status(500).json({error: "erro no servidor"});
-    }
+  try {
+    const users = await prisma.users.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: "erro no servidor" });
+  }
 };
 
 //function para buscar usuario especifico por id no banco de dados
 export const getSpecificUser = async (req, res) => {
-    try {
-        const{id} = req.params;
-        const user = await prisma.users.findFirst({
-            where: {
-                id: parseInt(id)
-            }
-        })
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({erro: "Erro interno no servidor"});
-        console.log(`erro: ${error.message}`);
-    }
+  try {
+    const { id } = req.params;
+    const user = await prisma.users.findFirst({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ erro: "Erro interno no servidor" });
+    console.log(`erro: ${error.message}`);
+  }
 };
 
 //function para inserir um usuario no banco de dados
-export const createUser = async (req, res) =>{
+/*export const createUser = async (req, res) =>{
     try{
         const{
             name,
@@ -48,43 +50,61 @@ export const createUser = async (req, res) =>{
         res.status(500).send("erro ao inserir no banco de dados");
         console.log(`erro: ${error.message}`);
     }
+};*/
+
+const JWT_SECRET = process.env.JWT_SECRET;
+export const createUser = async (req, res) => {
+  try {
+    const { name, age, email, password } = req.body;
+    // Fazer o hash
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await prisma.users.create({
+      data: {
+        name: name,
+        age: age,
+        email: email,
+        password: hashedPassword,
+      },
+    });
+    res.status(201).send("Usuario criado com exito!");
+  } catch (error) {
+    res.status(500).send("erro ao inserir no banco de dados");
+    console.log(`erro: ${error.message}`);
+  }
 };
 
 //function para atualizar um usuario
-export const updateUser = async (req, res) =>{
-    try {
-        const{id} = req.params;
-        const{
-            name,
-            age,
-            email,
-            password
-        } = req.body;
-        const userUpdated = await prisma.users.update({
-            where: { id: parseInt(id) },
-            data: { 
-                name: name,
-                age: age,
-                email: email,
-                password: password
-            }
-        });
-        res.status(200).send("usuario atualizado!");
-    } catch (error) {
-        res.status(500).send("Erro ao atualizar o usuario");
-        console.log(`erro: ${error.message}`);
-    }
+export const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, age, email, password } = req.body;
+    const userUpdated = await prisma.users.update({
+      where: { id: parseInt(id) },
+      data: {
+        name: name,
+        age: age,
+        email: email,
+        password: password,
+      },
+    });
+    res.status(200).send("usuario atualizado!");
+  } catch (error) {
+    res.status(500).send("Erro ao atualizar o usuario");
+    console.log(`erro: ${error.message}`);
+  }
 };
 
 //function para deletar um usuario do banco de dados
 export const deleteUser = async (req, res) => {
-  try{
-    const{id} = req.params;
+  try {
+    const { id } = req.params;
     const userDeleted = await prisma.users.delete({
-      where: { id: parseInt(id) }
+      where: { id: parseInt(id) },
     });
     res.status(204).send("Usuario deletada com sucesso");
-  }catch(error){
+  } catch (error) {
     res.status(500).send("erro ao deletar o usuario");
     console.log(`erro: ${error.message}`);
   }
