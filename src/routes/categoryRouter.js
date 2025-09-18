03179acf4503818d32397df3fc218267a6322f86
Diogoalves-dev <diogoribeirodev1@gmail>
2025-09-18 17:16:07 -0300

    feat: getSpecificCategoryByName

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { 
     getCategorys,
     getSpecificCategory,
+    getSpecificCategoryByName,
     createCategory,
     updateCategory,
     deleteCategory
@@ -11,6 +12,7 @@ const categoryRouter = Router();
 
 categoryRouter.get("/", getCategorys);
 categoryRouter.get("/:id", getSpecificCategory);
+categoryRouter.get("/nome/:name", getSpecificCategoryByName);
 categoryRouter.post("/", createCategory);
 categoryRouter.put("/:id", updateCategory);
 categoryRouter.delete("/:id", deleteCategory);
